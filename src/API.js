af8dce2a70816bea288afb518993f3448c283f91
arsen19899 <83328564+arsen19899@users.noreply.github.com>
2021-12-07 22:42:15 +0300

    Update API.js

--- src/API.js
+++ src/API.js
@@ -37,7 +37,6 @@ const apiSettings = {
     const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
     return await (await fetch(creditsEndpoint)).json();
   },
-  // Bonus material below for login
   getRequestToken: async () => {
     const reqToken = await (await fetch(REQUEST_TOKEN_URL)).json();
     return reqToken.request_token;
@@ -48,14 +47,12 @@ const apiSettings = {
       password,
       request_token: requestToken
     };
-    // First authenticate the requestToken
     const data = await (
       await fetch(LOGIN_URL, {
         ...defaultConfig,
         body: JSON.stringify(bodyData)
       })
     ).json();
-    // Then get the sessionId with the requestToken
     if (data.success) {
       const sessionId = await (
         await fetch(SESSION_ID_URL, {
@@ -66,18 +63,6 @@ const apiSettings = {
       return sessionId;
     }
   },
-  rateMovie: async (sessionId, movieId, value) => {
-    const endpoint = `${API_URL}movie/${movieId}/rating?api_key=${API_KEY}&session_id=${sessionId}`;
-
-    const rating = await (
-      await fetch(endpoint, {
-        ...defaultConfig,
-        body: JSON.stringify({ value })
-      })
-    ).json();
-
-    return rating;
-  }
-};
+ };
 
 export default apiSettings;
